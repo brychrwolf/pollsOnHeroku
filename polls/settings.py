18fb2894d8ea63cf495eb337f68a5f2b2a0de07f
Bryan Wolfford <bryan@wolfford.com>
2012-05-01 11:31:17 -1000

    settings.py once again

--- polls/settings.py
+++ polls/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/Initia7_B/Documents/HPU/CSCI 3771/pollsOnHeroku/polls', # Or path to database file if using sqlite3.
+        'NAME': '/app/polls', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
